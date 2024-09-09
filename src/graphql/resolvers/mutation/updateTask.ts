766d82e9d69185a2c16b49202eb71ff2386188dd
enkhtuya0511 <149783431+enkhtuya0511@users.noreply.github.com>
2024-09-10 03:13:58 +0800

    -.-

--- src/graphql/resolvers/mutation/updateTask.ts
+++ src/graphql/resolvers/mutation/updateTask.ts
@@ -3,7 +3,7 @@ import { UpdateTaskInput } from "@/graphql/generated/client";
 
 export async function updateTask(parent: any, { input }: { input: UpdateTaskInput }) {
   try {
-    const updatedTask = await TaskModel.findByIdAndUpdate(input._id, { ...input }, { new: true });
+    const updatedTask = await TaskModel.findByIdAndUpdate(input._id, input, { new: true });
 
     return updatedTask;
   } catch (error) {
